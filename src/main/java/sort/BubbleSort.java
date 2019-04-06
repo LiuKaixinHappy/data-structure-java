fd0fae0f97bdc3f20f642414927376b826de639e
LiuKaixin <644306737@qq.com>
2019-04-06 08:52:20 +0800

    add bubble sort

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package sort;
+
+
+public class BubbleSort {
+
+
+    public static int[] sort(int[] inputs) {
+        int swap = 0;
+        for (int j = 0; j < inputs.length; j++) {
+            boolean beSwapped = false;
+            for (int i = 0; i < inputs.length - 1 - j; i++) {
+                if (inputs[i] > inputs[i + 1]) {
+                    swap += 1;
+                    inputs[i] = inputs[i] ^ inputs[i + 1];
+                    inputs[i + 1] = inputs[i] ^ inputs[i + 1];
+                    inputs[i] = inputs[i] ^ inputs[i + 1];
+                    beSwapped = true;
+                }
+            }
+            if (!beSwapped) {
+                break;
+            }
+        }
+        System.out.printf("swap %d times in total\n", swap);
+        return inputs;
+    }
+}
